12c0112ee7e7efcdda6a0defebe9336d22d3dcc4
Aditya <2002adityaprakash@gmail.com>
2024-06-22 19:43:10 +0530

    Pagination and BlogCards are handled, next is page category

--- blogify/src/components/BlogPage.jsx
+++ blogify/src/components/BlogPage.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import BlogCards from './BlogCards';
+import Pagination from './Pagination';
 
 function BlogPage() {
     const [blogs,setBlogs]= useState([]);
+    const [currentPage,setCurrentPage]= useState(1);
+    const pageSize= 12;
+    const [selectedCategory, setSelectedCategory]= useState(null);
+    const [activeCategory, setActiveCategory]= useState(null);
 
     useEffect(()=>{
         async function fetchBlogs(){
-            let url= `1.32 time`;
+            let url= `http://localhost:5000/blogs?page=${currentPage}&limit=${pageSize  }`// & 1.51`;
+
+            //filter by category
+            if(selectedCategory){
+                url+= `&category=${selectedCategory}`
+            }
             const response= await fetch(url);
             const data= await response.json();
             console.log(data);
@@ -14,7 +24,18 @@ function BlogPage() {
         }
 
         fetchBlogs();
-    },[])
+    },[currentPage, pageSize, selectedCategory]);
+
+    //page changing btn
+    const handlePageChange=(pageNumber)=>{
+        setCurrentPage(pageNumber)
+    }
+
+    const handleCategoryChange=(category)=>{
+        setSelectedCategory(category);
+        setCurrentPage(1);
+        setActiveCategory(category)
+    }
   return (
     <div>
         {/* category section */}
@@ -22,11 +43,13 @@ function BlogPage() {
 
         {/* blogcards section */}
         <div>
-            <BlogCards blogs={}/>
+            <BlogCards blogs={blogs} currentPage={currentPage} selectedCategory={selectedCategory} pageSize={pageSize}/>
         </div>
 
         {/* pagination section */}
-        <div>Paginations</div>
+        <div>
+            <Pagination onPageChange={handlePageChange} currentPage={currentPage} blogs={blogs} pageSize={pageSize}/>
+        </div>
     </div>
   )
 }
